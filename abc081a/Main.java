9a194a5734456ce9ddddf96a02064689d0c91270
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-04-14 22:08:58 +0900

    abc081a

--- abc081a/Main.java
+++ abc081a/Main.java
@@ -3,12 +3,8 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        if ((a * b) % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
-        }
+        String s = sc.next();
+        s = s.replaceAll("0", "");
+        System.out.println(s.length());
     }
 }
